eaca5bb3824e58eb38cd9450cf2d33364c5e0bc3
bobbudowniczy <you@example.com>
2015-09-09 10:16:39 +0200

    register screen

--- app/src/main/java/com/example/pio/chatlibrary/login/RegisterActivity.java
+++ app/src/main/java/com/example/pio/chatlibrary/login/RegisterActivity.java
@@ -2,14 +2,25 @@ package com.example.pio.chatlibrary.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.example.pio.chatlibrary.R;
 
 /**
  * Created by pio on 09.09.15.
  */
 public class RegisterActivity extends AppCompatActivity {
 
+    public static final String TAG = RegisterActivity.class.getSimpleName();
+
+    public void regiser(View view) {
+        Log.d(TAG, "regiser");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
     }
 }
