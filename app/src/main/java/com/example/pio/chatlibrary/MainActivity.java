b0d5fd320d340958b1b93c4e701d82a39a9af9b3
bobbudowniczy <you@example.com>
2015-09-09 09:23:24 +0200

    zmiana package

--- app/src/main/java/com/example/pio/chatlibrary/MainActivity.java
+++ app/src/main/java/com/example/pio/chatlibrary/MainActivity.java
@@ -1,4 +1,4 @@
-package login;
+package com.example.pio.chatlibrary.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/example/pio/chatlibrary/login/LoginActivity.java
+++ app/src/main/java/com/example/pio/chatlibrary/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package login;
+package com.example.pio.chatlibrary.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 /**
  * Created by pio on 09.09.15.
  */
-public class RegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
